8ea14f862917ca365991696f9b6c783171d5818b
Dan Clarke <dan@dracan.co.uk>
2017-03-07 08:28:45 +0000

    Fix dir syntax

--- deploy.functions.cmd
+++ deploy.functions.cmd
@@ -80,7 +80,7 @@ FOR /F "tokens=*" %%i IN ('DIR /B %DEPLOYMENT_TARGET% /A:D') DO (
 
 :: 3. (Custom) Copy DanClarkeBlog.Core DLL
 
-ls \s \b %DEPLOYMENT_SOURCE%
+dir /s /b %DEPLOYMENT_SOURCE%
 echo Copying DanClarkeBlog.Core files from %DEPLOYMENT_SOURCE%\DanClarkeBlog.Core\bin\Release\
 echo To %DEPLOYMENT_TARGET%\ScheduledSync\
 xcopy /Y %DEPLOYMENT_SOURCE%\DanClarkeBlog.Core\bin\Release\*.dll %DEPLOYMENT_TARGET%\ScheduledSync\
