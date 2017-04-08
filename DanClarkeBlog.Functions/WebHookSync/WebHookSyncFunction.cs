39386a73c75c8c10088e128c724275e2311ceafd
Dan Clarke <dan@dracan.co.uk>
2017-04-08 08:09:47 +0100

    Add dropbox challenge request support

--- DanClarkeBlog.Functions/WebHookSync/WebHookSyncFunction.cs
+++ DanClarkeBlog.Functions/WebHookSync/WebHookSyncFunction.cs
@@ -1,28 +1,34 @@
 ﻿using System.Net;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using Microsoft.Azure.WebJobs.Host;
 
 namespace DanClarkeBlog.Functions.WebHookSync
 {
     public class WebHookSyncFunction
     {
-        /*
-        // This works for the Dropbox challenge request
-        var challenge = req.GetQueryNameValuePairs()
-            .FirstOrDefault(q => string.Compare(q.Key, "challenge", true) == 0)
-            .Value;
-
-        var res = req.CreateResponse(HttpStatusCode.OK);
-        res.Content = new StringContent(challenge, System.Text.Encoding.UTF8, "text/plain");
-        return res;
-        */
-
         public static HttpResponseMessage Run(HttpRequestMessage req, TraceWriter log, out string message)
         {
             //(todo) Verify Dropbox HMAC here (see X-Dropbox-Signature header)
             // Example in .NET here http://www.jerriepelser.com/blog/creating-a-dropbox-webhook-in-aspnet/
 
-            message = "OK";
+            // req.GetQueryNameValuePairs doesn't seem to exist in this version of the libraries - so use regex instead
+            var challengeMatch = Regex.Match(req.RequestUri.Query, @".*[?&]challenge=(.*?)[&$]");
+
+            if (challengeMatch.Success && challengeMatch.Groups.Count == 2)
+            {
+                log.Info("Received Dropbox challenge request");
+
+                var challenge = challengeMatch.Groups[1].Value;
+
+                message = "";
+
+                var res = req.CreateResponse(HttpStatusCode.OK);
+                res.Content = new StringContent(challenge, System.Text.Encoding.UTF8, "text/plain");
+                return res;
+            }
+
+            message = "INCREMENTAL_DROPBOX_UPDATE";
 
             return req.CreateResponse(HttpStatusCode.OK);
         }
