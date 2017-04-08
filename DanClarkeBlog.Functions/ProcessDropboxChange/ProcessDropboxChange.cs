a1b8b5c2f5af9fcf3f23e4fbc52f626f9c9bc1cc
Dan Clarke <dan@dracan.co.uk>
2017-04-08 07:59:13 +0100

    Further work on Azure Functions and dropbox incremental sync
    Started adding Dropbox webhook integration. We now have 3 Azure Functions - one for the scheduled full sync, one for the Dropbox webhook endpoint (which puts a message on a queue), and the other which takes the message on the queue to process an incremental sync.

---
+++ DanClarkeBlog.Functions/ProcessDropboxChange/ProcessDropboxChange.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using DanClarkeBlog.Core;
+using DanClarkeBlog.Core.Helpers;
+using DanClarkeBlog.Core.Repositories;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace DanClarkeBlog.Functions.ProcessDropboxChange
+{
+    public class ProcessDropboxChangeFunction
+    {
+        public static async Task Run(string message, TraceWriter log)
+        {
+            log.Info($"Found message on queue: {message}");
+
+            var settings = new Settings
+            {
+                DropboxAccessToken = Environment.GetEnvironmentVariable("DropboxAccessToken"),
+                BlogSqlConnectionString = Environment.GetEnvironmentVariable("BlogSqlConnectionString"),
+                AzureStorageConnectionString = Environment.GetEnvironmentVariable("AzureStorageConnectionString"),
+            };
+
+            var blogPostRenderer = new BlogPostMarkdownRenderer();
+            var blogPostSummaryHelper = new BlogPostSummaryHelper();
+            var imageRepository = new AzureImageRepository(settings);
+            var dropboxHelper = new DropboxHelper(settings, new HttpClientHelper());
+
+            var sourceRepo = new BlogPostDropboxRepository(blogPostRenderer, settings, blogPostSummaryHelper, imageRepository, dropboxHelper);
+            var destRepo = new BlogPostSqlServerRepository(settings);
+
+            var helper = new SyncHelper();
+
+            await helper.SynchronizeBlogPostsAsync(sourceRepo, destRepo, CancellationToken.None);
+
+            log.Info("Finished dropbox sync");
+        }
+    }
+}
--- DanClarkeBlog.Functions/WebHookSync/WebHookSyncFunction.cs
+++ DanClarkeBlog.Functions/WebHookSync/WebHookSyncFunction.cs
@@ -1,40 +1,30 @@
-﻿using System;
+﻿using System.Net;
 using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
-using DanClarkeBlog.Core;
-using DanClarkeBlog.Core.Helpers;
-using DanClarkeBlog.Core.Repositories;
 using Microsoft.Azure.WebJobs.Host;
 
 namespace DanClarkeBlog.Functions.WebHookSync
 {
     public class WebHookSyncFunction
     {
-        public static async Task Run(HttpRequestMessage req, TraceWriter log)
+        /*
+        // This works for the Dropbox challenge request
+        var challenge = req.GetQueryNameValuePairs()
+            .FirstOrDefault(q => string.Compare(q.Key, "challenge", true) == 0)
+            .Value;
+
+        var res = req.CreateResponse(HttpStatusCode.OK);
+        res.Content = new StringContent(challenge, System.Text.Encoding.UTF8, "text/plain");
+        return res;
+        */
+
+        public static HttpResponseMessage Run(HttpRequestMessage req, TraceWriter log, out string message)
         {
-            log.Info($"Starting Dropbox Sync ...");
+            //(todo) Verify Dropbox HMAC here (see X-Dropbox-Signature header)
+            // Example in .NET here http://www.jerriepelser.com/blog/creating-a-dropbox-webhook-in-aspnet/
 
-            var settings = new Settings
-            {
-                DropboxAccessToken = Environment.GetEnvironmentVariable("DropboxAccessToken"),
-                BlogSqlConnectionString = Environment.GetEnvironmentVariable("BlogSqlConnectionString"),
-                AzureStorageConnectionString = Environment.GetEnvironmentVariable("AzureStorageConnectionString"),
-            };
+            message = "OK";
 
-            var blogPostRenderer = new BlogPostMarkdownRenderer();
-            var blogPostSummaryHelper = new BlogPostSummaryHelper();
-            var imageRepository = new AzureImageRepository(settings);
-            var dropboxHelper = new DropboxHelper(settings, new HttpClientHelper());
-
-            var sourceRepo = new BlogPostDropboxRepository(blogPostRenderer, settings, blogPostSummaryHelper, imageRepository, dropboxHelper);
-            var destRepo = new BlogPostSqlServerRepository(settings);
-
-            var helper = new SyncHelper();
-
-            await helper.SynchronizeBlogPostsAsync(sourceRepo, destRepo, CancellationToken.None);
-
-            log.Info($"Finished dropbox sync");
+            return req.CreateResponse(HttpStatusCode.OK);
         }
     }
 }
