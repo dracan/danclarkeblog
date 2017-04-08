c39d8f12ff86807ee971d009430ea6d658f5d427
Dan Clarke <dan@dracan.co.uk>
2017-04-08 16:23:21 +0100

    Fixup nuget dependencies
    This now means that we can use GetQueryNameValuePairs, as it turns out it was in the Microsoft.AspNet.WebApi.Core nuget package, not the System.Net.Http one.

--- DanClarkeBlog.Functions/WebHookSync/WebHookSyncFunction.cs
+++ DanClarkeBlog.Functions/WebHookSync/WebHookSyncFunction.cs
@@ -1,6 +1,7 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Net.Http;
-using System.Text.RegularExpressions;
+using System.Linq;
 using Microsoft.Azure.WebJobs.Host;
 
 namespace DanClarkeBlog.Functions.WebHookSync
@@ -9,11 +10,20 @@ namespace DanClarkeBlog.Functions.WebHookSync
     {
         public static HttpResponseMessage Run(HttpRequestMessage req, TraceWriter log, out string message)
         {
+            var challenge = req.GetQueryNameValuePairs()
+                             .FirstOrDefault(q => String.Compare(q.Key, "challenge", StringComparison.OrdinalIgnoreCase) == 0)
+                             .Value;
+
+            log.Info($"CHALLENGE = {challenge}");
+
             //(todo) Verify Dropbox HMAC here (see X-Dropbox-Signature header)
             // Example in .NET here http://www.jerriepelser.com/blog/creating-a-dropbox-webhook-in-aspnet/
 
+            /*
             // req.GetQueryNameValuePairs doesn't seem to exist in this version of the libraries - so use regex instead
             var challengeMatch = Regex.Match(req.RequestUri.Query, @".*[?&]challenge=(.*?)[&$]");
+
+            log.Info("2");
 
             if (challengeMatch.Success && challengeMatch.Groups.Count == 2)
             {
@@ -23,13 +33,14 @@ namespace DanClarkeBlog.Functions.WebHookSync
 
                 message = "";
 
-                var res = new HttpResponseMessage(HttpStatusCode.OK)
+                return new HttpResponseMessage(HttpStatusCode.OK)
                           {
                               Content = new StringContent(challenge, System.Text.Encoding.UTF8, "text/plain")
                           };
-
-                return res;
             }
+
+            log.Info("3");
+            */
 
             message = "INCREMENTAL_DROPBOX_UPDATE";
 
