13382164139c3f3ab1565a09d361450571dfa2fc
Dan Clarke <dan@dracan.co.uk>
2017-04-09 18:56:33 +0100

    Fix image uri replacement issue when converting markdown

--- DanClarkeBlog.Core/Helpers/BlogPostMarkdownRenderer.cs
+++ DanClarkeBlog.Core/Helpers/BlogPostMarkdownRenderer.cs
@@ -13,7 +13,7 @@ namespace DanClarkeBlog.Core.Helpers
         {
             // The regular expression is to convert the relative path on the filesystem to path that will work on the web.
             // This allows editing the local Markdown files and being able to see the images in your markdown editor.
-            return Markdig.Markdown.ToHtml(Regex.Replace(markdown, @"(!\[.*?\]\()(.*?)\)", x => $"{x.Groups[1].Value}https://danclarkeblog.blob.core.windows.net/{x.Groups[2].Value})")); //(todo) Don't hardcode this
+            return Markdig.Markdown.ToHtml(Regex.Replace(markdown, @"(!\[.*?\]\()(.*?)\)", x => $"{x.Groups[1].Value}https://danclarkeblog.blob.core.windows.net/{x.Groups[2].Value.TrimStart('/')})")); //(todo) Don't hardcode this
         }
     }
 }
