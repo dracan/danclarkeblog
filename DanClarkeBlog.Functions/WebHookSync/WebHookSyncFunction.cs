5cd477890a2f4076120d7e860fbbc39c1722f732
Dan Clarke <dan@dracan.co.uk>
2017-04-09 21:48:07 +0100

    Change all projects to net46, and reference 4.0.0 version of system.net.http
    This is the only way I could get Azure Functions to work properly :( Also I've had to use a regular expression to parse the query string, as I couldn't get Azure Functions to find the GetQueryNameValuePairs method. At least this now works. Unfortunately the dotnet restore shows a bunch of downgrade warnings. Hopefully Azure Functions will soon support .net core, so I can upgrade again!

--- DanClarkeBlog.Functions/WebHookSync/WebHookSyncFunction.cs
+++ DanClarkeBlog.Functions/WebHookSync/WebHookSyncFunction.cs
@@ -3,6 +3,7 @@ using System.Net;
 using System.Net.Http;
 using System.Linq;
 using Microsoft.Azure.WebJobs.Host;
+using System.Text.RegularExpressions;
 
 namespace DanClarkeBlog.Functions.WebHookSync
 {
@@ -10,20 +11,11 @@ namespace DanClarkeBlog.Functions.WebHookSync
     {
         public static HttpResponseMessage Run(HttpRequestMessage req, TraceWriter log, out string message)
         {
-            var challenge = req.GetQueryNameValuePairs()
-                             .FirstOrDefault(q => String.Compare(q.Key, "challenge", StringComparison.OrdinalIgnoreCase) == 0)
-                             .Value;
-
-            log.Info($"CHALLENGE = {challenge}");
-
             //(todo) Verify Dropbox HMAC here (see X-Dropbox-Signature header)
             // Example in .NET here http://www.jerriepelser.com/blog/creating-a-dropbox-webhook-in-aspnet/
 
-            /*
             // req.GetQueryNameValuePairs doesn't seem to exist in this version of the libraries - so use regex instead
             var challengeMatch = Regex.Match(req.RequestUri.Query, @".*[?&]challenge=(.*?)[&$]");
-
-            log.Info("2");
 
             if (challengeMatch.Success && challengeMatch.Groups.Count == 2)
             {
@@ -38,9 +30,6 @@ namespace DanClarkeBlog.Functions.WebHookSync
                               Content = new StringContent(challenge, System.Text.Encoding.UTF8, "text/plain")
                           };
             }
-
-            log.Info("3");
-            */
 
             message = "INCREMENTAL_DROPBOX_UPDATE";
 
