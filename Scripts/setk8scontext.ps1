e29c1650f3708a8ad48d02f85994c59b3a02914d
Dan Clarke <dan@everstack.com>
2018-06-21 19:09:16 +0100

    Update setk8scontext.ps1 script
    On my old laptop, I had to use `docker-credential-gcr configure-docker` instead of `gcloud auth configure-docker`, as the latter errored. Seems to work fine on my new one.

--- Scripts/setk8scontext.ps1
+++ Scripts/setk8scontext.ps1
@@ -1,3 +1,4 @@
 k config use-context blog
 gcloud config configurations activate everstack
-docker-credential-gcr configure-docker
+gcloud auth configure-docker
+# docker-credential-gcr configure-docker
