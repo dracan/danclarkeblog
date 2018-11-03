fe71183f9337810a385750df7524d33b48d8739e
Dan <dan@nospam.com>
2018-11-03 07:23:04 +0000

    Update GitVersion.sh to also update Azure Pipelines build number
    The source tagging is in a different scope to the variable numbers, so git tagging wasn't working otherwise ...
    https://github.com/GitTools/GitVersion/issues/1270

--- GitVersion.sh
+++ GitVersion.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 output=$(dotnet ./GitVersion/GitVersion.dll | grep -Eo "\"MajorMinorPatch\":\"([0-9]+\.[0-9]+\.[0-9]+)\"" | cut -d: -f2)
+output=${output//\"}
 echo "Setting version number to ${output}"
-echo "##vso[task.setvariable variable=VersionNumber]${output//\"}"
+echo "##vso[task.setvariable variable=VersionNumber]${output}"
+echo "##vso[build.updatebuildnumber]${output}"
