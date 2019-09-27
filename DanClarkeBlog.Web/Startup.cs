80f2d1253d39497363ad3d04a66278e1b9bb1086
Dan Clarke <dan@nospam.com>
2019-09-27 08:36:53 +0100

    Add application insights to web project

--- DanClarkeBlog.Web/Startup.cs
+++ DanClarkeBlog.Web/Startup.cs
@@ -31,7 +31,8 @@ namespace DanClarkeBlog.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public IServiceProvider ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
+            services.AddApplicationInsightsTelemetry();
+
             services.AddMvc(config =>
                 {
                     config.RespectBrowserAcceptHeader = true;
