b1760cde9cac428952bf247a71d8cf10558ce40e
Dan Clarke <dan@nospam.com>
2020-05-28 07:05:55 +0100

    Search results should not include draft posts

--- DanClarkeBlog.Core/Helpers/AzureSearchHelper.cs
+++ DanClarkeBlog.Core/Helpers/AzureSearchHelper.cs
@@ -21,24 +21,24 @@ namespace DanClarkeBlog.Core.Helpers
             using (var serviceClient = new SearchServiceClient(_settings.AzureSearchInstanceName, new SearchCredentials(_settings.AzureSearchKey)))
             using (var indexClient = serviceClient.Indexes.GetClient(_settings.AzureSearchIndexName))
             {
-                var results = await indexClient.Documents.SearchAsync(searchTerm, new SearchParameters
+                var results = (await indexClient.Documents.SearchAsync(searchTerm, new SearchParameters
                 {
-                    Select = new[] {"Id", "Title", "Route", "PublishDate", "HtmlShortText"},
+                    Select = new[] {"Id", "Title", "Route", "PublishDate", "HtmlShortText", "Published"},
                     Skip = offset,
                     Top = count,
                     IncludeTotalResultCount = true,
-                });
+                })).Results.Where(x => (bool) x.Document["Published"]).ToList();
 
                 return new BlogPostListing
                 {
-                    Posts = results.Results.Select(x => new BlogPost
+                    Posts = results.Select(x => new BlogPost
                     {
                         Id = Guid.Parse((string) x.Document["Id"]),
                         Title = (string) x.Document["Title"],
                         Route = (string) x.Document["Route"],
                         HtmlShortText = (string) x.Document["HtmlShortText"],
                     }).ToList(),
-                    TotalPosts = (int) (results.Count ?? 0),
+                    TotalPosts = results.Count,
                 };
             }
         }
